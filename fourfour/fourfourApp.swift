bc2628712f2dabfa21653b16d9829ecb91403d33
Callan Murphy <callanmurphy31@gmail.com>
2021-05-27 02:04:24 -0400

    Initial Commit

---
+++ fourfour/fourfourApp.swift
@@ -0,0 +1,17 @@
+//
+//  fourfourApp.swift
+//  fourfour
+//
+//  Created by callan on 2021-05-27.
+//
+
+import SwiftUI
+
+@main
+struct fourfourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
